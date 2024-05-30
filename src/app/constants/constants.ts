eb3c96ab820ec79050e5cb50c1cc6f46459d9dea
= <hp452444@gmail.com>
2024-05-30 09:33:18 +0530

    Comments added

--- src/app/constants/constants.ts
+++ src/app/constants/constants.ts
@@ -1,2 +1,6 @@
 export const VALID_RESPONSE = '1';
 // Other constants will be defined here
+
+// The benefits of using constants are :
+// 1. There will not be any typo while working with the strings
+// 2. Any changes in strings in future will be handled easily, no need to update strings in all the files
